f1d195546bc1decc99303c6d310f1d1008339027
Akshaiya Sakthivel <120248925+AkshaiyaSakthivel003@users.noreply.github.com>
2025-07-17 18:08:47 +0400

    Delete Day8 directory

--- Day8/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import movieReducer from './movieReducer';
-
-const rootReducer = combineReducers({
-  favorites: movieReducer,
-});
-
-export default rootReducer;
